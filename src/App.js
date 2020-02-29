98ba5bb481f2436696e6650eb78b186ff83f6f58
Austin Wright <awguitar86@gmail.com>
2020-02-29 08:53:16 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CalendarBackground from './Components/CalendarBackground/CalendarBackground';
+
+function App() {
+  return (
+    <div className="App">
+      <CalendarBackground />
+    </div>
+  );
+}
+
+export default App;
