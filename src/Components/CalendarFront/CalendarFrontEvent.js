191079840eca2b2dab45525776947585b7a6da69
Austin Wright <awguitar86@gmail.com>
2020-04-01 11:38:29 -0600

    worked on the modal

--- src/Components/CalendarFront/CalendarFrontEvent.js
+++ src/Components/CalendarFront/CalendarFrontEvent.js
@@ -16,16 +16,17 @@ function CalendarFrontEvent(props) {
     day
   } = props;
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-  const closeModal = () => {
-    setIsModalOpen(false);
-    console.log(isModalOpen);
-  };
-  // console.log(isModalOpen);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  console.log(modalIsOpen);
   return (
     <div
       className="cal-front-item"
@@ -40,9 +41,8 @@ function CalendarFrontEvent(props) {
       <p>{event.instructor}</p>
       <p>{event.meetingPattern}</p>
       <Modal
-        isOpen={isModalOpen}
+        isOpen={modalIsOpen}
         onRequestClose={closeModal}
-        shouldCloseOnOverlayClick={true}
         contentLabel="Course Details"
         className="course-modal"
         overlayClassName="course-modal-overlay"
@@ -50,17 +50,54 @@ function CalendarFrontEvent(props) {
       >
         <div className="course-modal-wrap">
           <div className="modal-header">
+            <h3>Event Details</h3>
             <button className="modal-close-x" onClick={closeModal}>
               X
             </button>
           </div>
-          <p>Course Title: {event.courseTitle}</p>
-          <p>Instructor: {event.instructor}</p>
-          <p>Location: {event.location}</p>
-          <p>Meeting Pattern: {event.meetingPattern}</p>
-          <p>Block: {event.block}</p>
-          <p>Credit Hours{event.creditHours}</p>
-          <p>Class Id: {event.classId}</p>
+          <div className="modal-body">
+            <div className="modal-body-left">
+              <label htmlFor="course-title">
+                Course
+                <input
+                  type="text"
+                  id="course-title"
+                  value={event.courseTitle}
+                />
+              </label>
+              <label htmlFor="course-id">
+                Course ID
+                <input type="text" id="course-id" value={event.classId} />
+              </label>
+              <label htmlFor="instructor">
+                Instructor
+                <input type="text" id="instructor" value={event.instructor} />
+              </label>
+              <label htmlFor="location">
+                Location
+                <input type="text" id="location" value={event.location} />
+              </label>
+            </div>
+            <div className="modal-body-right">
+              <label htmlFor="credit-hours">
+                Credit Hours
+                <input
+                  type="text"
+                  id="credit-hours"
+                  value={event.creditHours}
+                />
+              </label>
+              <label htmlFor="block">
+                Block
+                <input type="text" id="block" value={event.block} />
+              </label>
+              <p>Meeting Pattern: {event.meetingPattern}</p>
+            </div>
+          </div>
+          <div className="modal-btns">
+            <button className="cancel-btn">CANCEL</button>
+            <button className="save-btn">SAVE</button>
+          </div>
         </div>
       </Modal>
     </div>
