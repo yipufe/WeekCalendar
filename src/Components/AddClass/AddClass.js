c63968096efd39c6a3bf43807efa7edbea1b63d3
Austin Wright <awguitar86@gmail.com>
2020-07-23 22:50:07 -0600

    removed useEffect

--- src/Components/AddClass/AddClass.js
+++ src/Components/AddClass/AddClass.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './addClass.scss';
 import Select from 'react-select';
 import { selectDays, selectTimes } from '../../calendarDaysAndTimesData';
