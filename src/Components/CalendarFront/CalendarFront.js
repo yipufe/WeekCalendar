4855e8ef6b535e71908a9e577cfaf8eae10b9652
Austin Wright <awguitar86@gmail.com>
2020-07-27 21:44:16 -0600

    changes

--- src/Components/CalendarFront/CalendarFront.js
+++ src/Components/CalendarFront/CalendarFront.js
@@ -3,7 +3,6 @@ import './calendarFront.scss';
 import styled from 'styled-components';
 import { calDays, calTimes, colors } from '../../calendarDaysAndTimesData';
 
-
 function CalendarFront(props) {
   const { displayData, initialAndChangedData } = props; // This is filtering through the displayData and filtering out all the data that say "Does Not Meet"
   let meetingPatternArr;
@@ -18,24 +17,15 @@ function CalendarFront(props) {
   }
 
   const Container = styled.div`
-  width: 85%;
-  background-color: ${(props) => colors[props.index]};
-  color: black;
-  border-radius: 5px;
-  padding: 5px;
-  cursor: pointer;
+    width: 85%;
+    background-color: ${(props) => colors[props.index]};
+    color: black;
+    border-radius: 5px;
+    padding: 5px;
+    cursor: pointer;
   `;
 
-  const Container = styled.div`
-	width: 85%;
-	background-color: ${(props) => colors[props.index]};
-	color: black;
-	border-radius: 5px;
-	padding: 5px;
-	cursor: pointer;
-  `;
-
-  const eventData = meetingPatternArr.map(event => {
+  const eventData = meetingPatternArr.map((event) => {
     const days = event.meetingPattern.split(' ')[0];
     const dayArray = days !== 'Sa' ? days.split('') : ['Sa'];
     const startTime = event.meetingPattern.split(' ')[1].split('-')[0];
@@ -43,18 +33,20 @@ function CalendarFront(props) {
 
     const displayEvents = dayArray.map((day) => {
       return (
-		<Container
-			key={`${day}-${event.classId}`}
-			index={meetingPatternArr.indexOf(event)}
-			style={{
-				gridColumn: `${calDays[day]}`,
-        		gridRow: `${calTimes[startTime]} / ${calTimes[endTime]}`,
-			}}
-		>
-			<p class="cal-front-item-p">{event.course}</p>
-			<p class="cal-front-item-p">{event.courseTitle.substring(0,15) + '...'}</p>
-			<p class="cal-front-item-p">{event.meetingPattern}</p>
-		</Container>
+        <Container
+          key={`${day}-${event.classId}`}
+          index={meetingPatternArr.indexOf(event)}
+          style={{
+            gridColumn: `${calDays[day]}`,
+            gridRow: `${calTimes[startTime]} / ${calTimes[endTime]}`,
+          }}
+        >
+          <p class="cal-front-item-p">{event.course}</p>
+          <p class="cal-front-item-p">
+            {event.courseTitle.substring(0, 15) + '...'}
+          </p>
+          <p class="cal-front-item-p">{event.meetingPattern}</p>
+        </Container>
       );
     });
 
